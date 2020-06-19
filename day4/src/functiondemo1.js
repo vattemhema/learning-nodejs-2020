37b504d14a9311290782b978745c8b00db293e2b
Vattem Hema <vattem.hema@gmail.com>
2020-06-19 18:02:06 +0530

    Initial Version

---
+++ day4/src/functiondemo1.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Description : A function which accepts nothing and returns nothing//
+
+const displayMessage = function () {
+    console.log('Hello World');
+}
+
+displayMessage();
+displayMessage();
+displayMessage();
+displayMessage();
+displayMessage();
---
+++ day4/src/functiondemo3.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// A function which accepts something but returns nothing //
+
+const greetPerson = function (personname) {
+    console.log(`Hello ${personname}. Welcome to NodeJS`);
+
+}
+
+greetPerson('Hema');
+
+greetPerson('Revathi');
+
+greetPerson('Guru');
+
+greetPerson('Vignesh');
